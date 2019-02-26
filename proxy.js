0b967d5317fe9742edca072d8b6ebc6ccf678b57
Maxime Fleurant <maximefleurant.exe@gmail.com>
2019-02-26 21:07:06 +0100

    qsdqs

--- proxy.js
+++ proxy.js
@@ -1,15 +1,22 @@
-
 var http = require('http'),
 httpProxy = require('http-proxy');
 
-httpProxy.createServer(function (req, res, proxy) {
+// httpProxy.createServer(function (req, res, proxy) {
+//   console.log(req.headers.host);
+// if(req.headers.host == 'thebrutalistdb.com') {
+//   proxy.proxyRequest(req, res, {
+//     host: 'http://thebrutalistdb.com',
+//     port: 3000
+//   });
+// } else {
+//     res.end('Site em manutenção.');
+// }
+// }).listen(80);
 
-if(req.headers.host == 'thebrutalistdb.com') {
-  proxy.proxyRequest(req, res, {
-    host: 'http://thebrutalistdb.com',
-    port: 3000
-  });
-} else {
-    res.end('Site em manutenção.');
-}
+var proxy = httpProxy.createProxyServer({});
+
+http.createServer(function(req, res) {
+  
+  console.log(req.headers.host)
+  // proxy.web(req, res, { target: 'http://127.0.0.1:5060' });
 }).listen(80);
