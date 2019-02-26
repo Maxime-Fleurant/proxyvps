02ec3b8c611319765d97dff42b4decdde01a150f
Maxime Fleurant <maximefleurant.exe@gmail.com>
2019-02-26 20:53:52 +0100

    sd

---
+++ proxy.js
@@ -0,0 +1,15 @@
+
+var http = require('http'),
+httpProxy = require('http-proxy');
+
+httpProxy.createServer(function (req, res, proxy) {
+
+if(req.headers.host == 'thebrutalistdb.com') {
+  proxy.proxyRequest(req, res, {
+    host: 'localhost',
+    port: 3000
+  });
+} else {
+    res.end('Site em manutenção.');
+}
+}).listen(80);
